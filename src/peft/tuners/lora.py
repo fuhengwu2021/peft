990af9b3b84c0dd693b5c4883b848c3794a04b15
Fuheng Wu <fuheng.wu@oracle.com>
2023-04-09 13:01:38 -0700

    working now!

--- src/peft/tuners/lora.py
+++ src/peft/tuners/lora.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import math
 import re
 from dataclasses import asdict, dataclass, field
 from enum import Enum
@@ -20,10 +19,10 @@ from typing import List, Optional, Union
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from gptq import QuantLinear
 
 from ..utils import PeftConfig, PeftType
+
 
 @dataclass
 class LoraConfig(PeftConfig):
@@ -53,6 +52,8 @@ class LoraConfig(PeftConfig):
         },
     )
     lora_alpha: int = field(default=None, metadata={"help": "Lora alpha"})
+    bits: int = field(default=4, metadata={"help": "quantization bits number/width"})
+    max_lora_layers: int = field(default=10, metadata={"help": "max number of lora layers"})
     lora_dropout: float = field(default=None, metadata={"help": "Lora dropout"})
     merge_weights: bool = field(
         default=False, metadata={"help": "Merge weights of the original model and the Lora model"}
@@ -77,36 +78,16 @@ class LoraConfig(PeftConfig):
 
 
 class LoraModel(torch.nn.Module):
-    """
-    Creates Low Rank Adapter (Lora) model from a pretrained transformers model.
-
-    Args:
-        model ([`transformers.PreTrainedModel`]): The model to be adapted.
-        config ([`LoraConfig`]): The configuration of the Lora model.
-
-    Returns:
-        `torch.nn.Module`: The Lora model.
-
-    Example::
-
-        >>> from transformers import AutoModelForSeq2SeqLM, LoraConfig
-        >>> from peft import LoraModel, LoraConfig
-        >>> config = LoraConfig(peft_type="LORA", task_type="SEQ_2_SEQ_LM", r=8, lora_alpha=32, target_modules=["q", "v"], lora_dropout=0.01)
-        >>> model = AutoModelForSeq2SeqLM.from_pretrained("t5-base") >>> lora_model = LoraModel(config, model)
-
-    **Attributes**:
-        - **model** ([`transformers.PreTrainedModel`]) -- The model to be adapted.
-        - **peft_config** ([`LoraConfig`]): The configuration of the Lora model.
-    """
-    def __init__(self, config, model, wbits=4):
+    def __init__(self, config, model, bits=None):
         super().__init__()
         self.peft_config = config
         self.model = model
-        self._find_and_replace(wbits)
+        self.bits = bits or config.bits
+        self._find_and_replace()
         mark_only_lora_as_trainable(self.model, self.peft_config.bias)
         self.forward = self.model.forward
 
-    def _find_and_replace(self, wbits):
+    def _find_and_replace(self):
         is_target_modules_in_base_model = False
         is_hf_device_map_available = hasattr(self.model, "hf_device_map")
         kwargs = {
@@ -117,22 +98,26 @@ class LoraModel(torch.nn.Module):
             "merge_weights": (self.peft_config.merge_weights or self.peft_config.inference_mode)
             and not is_hf_device_map_available,
         }
+        count = 0
         for key, _ in self.model.named_modules():
             if isinstance(self.peft_config.target_modules, str):
                 target_module_found = re.fullmatch(self.peft_config.target_modules, key)
             else:
                 target_module_found = any(key.endswith(target_key) for target_key in self.peft_config.target_modules)
 
-            if target_module_found:
+            if target_module_found and count < self.peft_config.max_lora_layers:
                 if not is_target_modules_in_base_model:
                     is_target_modules_in_base_model = True
                 parent, target, target_name = self.__get_submodules(key)
                 kwargs.update({"enable_lora": [True], "device": target.qweight.device})
-                new_module = MergedQuantLinear(target.qweight.shape[0]*(32//wbits), target.bias.shape[0], wbits, **kwargs)
+                new_module = MergedQuantLinear(
+                    (target.qweight.shape[0] * 32) // self.bits, target.bias.shape[0], self.bits, **kwargs
+                )
                 new_module.scales = target.scales
                 new_module.zeros = target.zeros
                 new_module.bias = target.bias
                 self._replace_module(parent, target_name, new_module, target)
+                count += 1
         if not is_target_modules_in_base_model:
             raise ValueError(
                 f"Target modules {self.peft_config.target_modules} not found in the base model. "
@@ -188,7 +173,6 @@ class LoraModel(torch.nn.Module):
         self._set_adapter_layers(enabled=False)
 
 
-
 # Below code is based on https://github.com/microsoft/LoRA/blob/main/loralib/layers.py
 # and modified to work with PyTorch FSDP
 
@@ -241,19 +225,29 @@ class LoraLayer:
 
 class MergedQuantLinear(QuantLinear, LoraLayer):
     # Lora implemented in a dense layer with QuantLinear
-    def __init__(self, in_features: int, out_features: int, wbits = 4, r: int = 0, lora_alpha: int = 1, lora_dropout: float = 0.0, enable_lora: List[bool] = [False], **kwargs):
-        QuantLinear.__init__(self, wbits, in_features, out_features)
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bits=4,
+        r: int = 0,
+        lora_alpha: int = 1,
+        lora_dropout: float = 0.0,
+        enable_lora: List[bool] = [True],
+        **kwargs,
+    ):
+        QuantLinear.__init__(self, bits, in_features, out_features)
         self.in_features = in_features
         self.out_features = out_features
         LoraLayer.__init__(self, r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout, merge_weights=False)
-        #if out_features % len(enable_lora) != 0:
+        # if out_features % len(enable_lora) != 0:
         #    raise ValueError("The length of enable_lora must divide out_features")
         self.enable_lora = enable_lora
-        dev = kwargs['device'] if 'device' in kwargs else torch.device('cpu')
+        dev = kwargs["device"] if "device" in kwargs else torch.device("cpu")
         # Actual trainable parameters
         if r > 0 and any(enable_lora):
-            self.lora_A = nn.Linear(in_features, r, bias=False)
-            self.lora_B = nn.Linear(r, out_features, bias=False)
+            self.lora_A = nn.Linear(in_features, r, bias=False, dtype=torch.float32)
+            self.lora_B = nn.Linear(r, out_features, bias=False, dtype=torch.float32)
             self.scaling = self.lora_alpha / self.r
             # Freezing the pre-trained weight matrix
             self.qweight.requires_grad = False
@@ -262,14 +256,19 @@ class MergedQuantLinear(QuantLinear, LoraLayer):
     def reset_parameters(self):
         if hasattr(self, "lora_A"):
             # initialize A the same way as the default for nn.Linear and B to zero
-            nn.init.kaiming_uniform_(self.lora_A.weight, a=math.sqrt(5))
+            # nn.init.kaiming_uniform_(self.lora_A.weight, mode='fan_in', nonlinearity='relu', a=0.01414)
+            nn.init.xavier_uniform_(self.lora_A.weight)
             nn.init.zeros_(self.lora_B.weight)
 
     def forward(self, x: torch.Tensor):
         #import pudb; pu.db
         result = super().forward(x)
         if self.r > 0:
-            drp = self.lora_dropout(x)            
-            output = self.lora_B(self.lora_A(drp)) * self.scaling
+            drp = self.lora_dropout(x)
+            tmp_a = self.lora_A(drp)
+            tmp_b = self.lora_B(tmp_a)
+            output = tmp_b * self.scaling
             result += output
+        if torch.any(torch.isnan(result)):
+            print("🔥")
         return result
